41780e483d3517db12c9c19ef320eeb916030ed2
eglumov <eugen.glumov@yandex.ru>
2026-06-08 00:24:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2022 NNTU-CS
+#include <chrono>
+#include <iostream>
+#include <vector>
 #include "tree.h"
 
 int main() {
-  return 0;
+    std::vector<char> in = {'1', '2', '3'};
+    Tree tree(in);
+
+    auto p1 = getPerm1(tree, 2);
+    auto p2 = getPerm2(tree, 2);
+
+    for (char c : p1) std::cout << c;
+    std::cout << std::endl;
+    for (char c : p2) std::cout << c;
+    std::cout << std::endl;
+
+    return 0;
 }
